2e8e3150e7f75fd67e68f3e4f534cf26135b1e33
konok101 <rockykonok@gmail.com>
2022-05-12 12:49:51 +0600

    local storage to add and sisplay from local storage part done

--- Shoppin-localStorage/shopping.js
+++ Shoppin-localStorage/shopping.js
@@ -1,6 +1,21 @@
+/* Display loacal storage part */
+
+const displayLocalStorageCart = () => {
+    const cart = getCart();
+    for(const name in cart){
+        displayProduct(name);
+    }
+}
+
+
+
+//Add item  or productsa
 const addItem= () =>{
 const nameField= document.getElementById("product-name");
 const name= nameField.value;
+if(!name){
+    return;
+}
 
 //display in ui
 displayProduct(name);
@@ -37,7 +52,20 @@ const getCart =()=>{
 
 const addProductToCart = name => {
     const cart = getCart();
-    cart[name] =1;
+  if(cart[name]){
+      cart[name]= cart[name]+1;
+  }
+  else{
+      cart[name]=1;
+  }
     const cartStringfied = JSON.stringify(cart);
     localStorage.setItem('cart', cartStringfied);
 }
+
+const placeOrder = ()=>{
+    document.getElementById('products').textContent='';
+    localStorage.removeItem('cart');
+
+}
+
+displayLocalStorageCart();
